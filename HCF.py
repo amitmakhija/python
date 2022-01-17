39683dd25cbd5140d5e2eb262dba3cd8be589520
Amit Makhija <makhija.amit@gmail.com>
2022-01-17 12:31:34 +0530

    Create HCF.py

---
+++ HCF.py
@@ -0,0 +1,20 @@
+'''
+This program takes the list of numbers and finds the HCF of all the those numbers
+'''
+
+
+def main(a,b):
+    if a == 0:
+        return b
+    else:
+        return main(b%a,a)
+
+if __name__ == '__main__':
+    import sys
+    sys.setrecursionlimit(150000)
+    N = list(map(int,list(input("Enter two numbers separated by space : ").split())))
+    a = main(N[0],N[1])
+    for i in range(len(N)-1):
+        a = main(a,N[i+1])
+        
+    print(a)
