2e5772d815a280ed724ba4464d20d8a05fa6d21b
Amit Makhija <makhija.amit@gmail.com>
2022-02-24 21:13:00 +0530

    update

--- sliding_window_subarray_unique_count.py
+++ sliding_window_subarray_unique_count.py
@@ -4,31 +4,31 @@ in a subarray of size K
 '''
 
 
-lst = [6,3,7,3,8,6,9]
-k = 3
+A = [1, 2, 1, 3, 4, 3]
+B = 3
 
-def solve(lst,k):
+def solve(A,B):
     ans = []
     freq_dict = dict()
-    for i in range(k):
-        if lst[i] not in freq_dict:
-            freq_dict[lst[i]] = 1
+    for i in range(B):
+        if A[i] not in freq_dict:
+            freq_dict[A[i]] = 1
         else:
-            freq_dict[lst[i]] += 1
+            freq_dict[A[i]] += 1
     ans.append(len(freq_dict))
-    for i in range(k,len(lst)):
-        freq_dict[lst[i-k]] -=1
-        if freq_dict[lst[i-k]] == 0:
-            del(freq_dict[lst[i-k]])
+    for i in range(B,len(A)):
+        freq_dict[A[i-B]] -=1
+        if freq_dict[A[i-B]] == 0:
+            del(freq_dict[A[i-B]])
         
-        if lst[i] not in freq_dict:
-            freq_dict[lst[i]] = 1
+        if A[i] not in freq_dict:
+            freq_dict[A[i]] = 1
         else:
-            freq_dict[lst[i]] += 1
+            freq_dict[A[i]] += 1
         
         ans.append(len(freq_dict))
 
     
     return ans
 
-print(solve(lst,k))
+print(solve(A,B))
